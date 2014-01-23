f60cd3f4f40028747d66da4ef2d939f1ed943b46
pranavvishnu <pranavvishnur@gmail.com>
2014-01-23 12:10:42 -0800

    Bug Fixes and design updates
    Currently fully functional.

--- src/clean-watch.c
+++ src/clean-watch.c
@@ -19,7 +19,7 @@ void handle_minute_tick(struct tm *tick_time, TimeUnits units_changed) {
 	static char min_text[] = "  ";
 	static char hour_text[] = "  ";
     static char date_text[] = "Xxxxxxxxx 00";
-	static char day_text[] = "M";
+	static char day_text[] = "Mm";
 	
 	char *time_format;
 
@@ -27,6 +27,8 @@ void handle_minute_tick(struct tm *tick_time, TimeUnits units_changed) {
   // TODO: Only update the date when it's changed.
   strftime(date_text, sizeof(date_text), "%B %e", tick_time);
   text_layer_set_text(text_date_layer, date_text);
+  strftime(day_text, sizeof(day_text), "%a", tick_time);
+  text_layer_set_text(text_day_layer, day_text);
 	
   if (clock_is_24h_style()) {
     time_format = "%R";
@@ -37,19 +39,24 @@ void handle_minute_tick(struct tm *tick_time, TimeUnits units_changed) {
   strftime(time_text, sizeof(time_text), time_format, tick_time);
 
   // Kludge to handle lack of non-padded hour format string
-  // for twelve hour clock.
+  // for twelve hour clock and split time into hours/mins.
   if (!clock_is_24h_style() && (time_text[0] == '0')) {
-    memmove(time_text, &time_text[1], sizeof(time_text) - 1);
+	  int l = sizeof(hour_text)/sizeof(hour_text[0]);
+	  for (int i=0; i < l; i++){
+		  hour_text[i] = 0;
+	  }
+	  text_layer_set_text_alignment(text_hour_layer, GTextAlignmentRight);
+      memmove(time_text, &time_text[1], sizeof(time_text) - 1);
 	  memmove(hour_text, &time_text[0], 1);
 	  memmove(min_text, &time_text[2], 2);
   } else{
+	  text_layer_set_text_alignment(text_hour_layer, GTextAlignmentCenter);
 	  memmove(hour_text, &time_text[0], 2);
 	  memmove(min_text, &time_text[3], 2);
   }
 	
   text_layer_set_text(text_hour_layer, hour_text);
   text_layer_set_text(text_min_layer, min_text);
-  text_layer_set_text(text_day_layer, day_text);
 }
 
 void handle_deinit(void) {
@@ -62,25 +69,32 @@ void handle_deinit(void) {
 }
 
 void handle_init(void) {
+	
+  // Create the window	
   window = window_create();
   window_stack_push(window, true /* Animated */);
   window_set_background_color(window, GColorBlack);
 
   Layer *window_layer = window_get_root_layer(window);
-
-  text_date_layer = text_layer_create(GRect(8, 40, 144-8, 168-40));
+	
+  ////Text Layers
+  
+  //Date Layer
+  text_date_layer = text_layer_create(GRect(8, 10, 144-8, 168-10));
   text_layer_set_text_color(text_date_layer, GColorWhite);
   text_layer_set_background_color(text_date_layer, GColorClear);
   text_layer_set_font(text_date_layer, fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_HELVETICA_LIGHT_18)));
   layer_add_child(window_layer, text_layer_get_layer(text_date_layer));
-
+	
+  //Hour Layer
   text_hour_layer = text_layer_create(GRect(0, 91, 90, 168-91));
   text_layer_set_text_color(text_hour_layer, GColorBlack);
   text_layer_set_background_color(text_hour_layer, GColorWhite);
 	text_layer_set_text_alignment(text_hour_layer, GTextAlignmentCenter);
   text_layer_set_font(text_hour_layer, fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_HELVETICA_LIGHT_65)));
   layer_add_child(window_layer, text_layer_get_layer(text_hour_layer));
-	
+
+  //Minutes Layer
   text_min_layer = text_layer_create(GRect(90, 91, 144-90, 168-91));
   text_layer_set_text_color(text_min_layer, GColorBlack);
   text_layer_set_background_color(text_min_layer, GColorWhite);
@@ -88,23 +102,26 @@ void handle_init(void) {
   text_layer_set_font(text_min_layer, fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_HELVETICA_LIGHT_43)));
   layer_add_child(window_layer, text_layer_get_layer(text_min_layer));
 	
-  text_day_layer = text_layer_create(GRect(90, 138, 144-90, 168-138));
+  //Day of week
+  text_day_layer = text_layer_create(GRect(90, 140, 144-90, 168-140));
   text_layer_set_text_color(text_day_layer, GColorBlack);
-  text_layer_set_background_color(text_day_layer, GColorWhite);
+  text_layer_set_background_color(text_day_layer, GColorClear);
   text_layer_set_text_alignment(text_day_layer, GTextAlignmentCenter);
   text_layer_set_font(text_day_layer, fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_HELVETICA_LIGHT_18)));
   layer_add_child(window_layer, text_layer_get_layer(text_day_layer));
 
-  GRect line_frame = GRect(8, 68, 139, 2);
+  GRect line_frame = GRect(8, 38, 139, 2);
   line_layer = layer_create(line_frame);
   layer_set_update_proc(line_layer, line_layer_update_callback);
   layer_add_child(window_layer, line_layer);
 
   tick_timer_service_subscribe(MINUTE_UNIT, handle_minute_tick);
+	
   // Avoids a blank screen on watch start.
   time_t now = time(NULL);
   struct tm *tick_time = localtime(&now);
   handle_minute_tick(tick_time, MINUTE_UNIT);
+  
 }
 
 
